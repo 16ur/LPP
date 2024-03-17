050029fc5620601f0586d98fc764b508a5e65b28
16ur <akuseruuu@protonmail.com>
2024-03-17 10:14:28 +0100

    Add icon to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet } from '@ionic/angular/standalone';
+import {
+  IonApp,
+  IonSplitPane,
+  IonMenu,
+  IonContent,
+  IonList,
+  IonListHeader,
+  IonNote,
+  IonMenuToggle,
+  IonItem,
+  IonIcon,
+  IonLabel,
+  IonRouterOutlet,
+} from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
-import { register} from 'swiper/element/bundle';
+import {
+  mailOutline,
+  mailSharp,
+  paperPlaneOutline,
+  paperPlaneSharp,
+  heartOutline,
+  heartSharp,
+  archiveOutline,
+  archiveSharp,
+  trashOutline,
+  trashSharp,
+  warningOutline,
+  warningSharp,
+  bookmarkOutline,
+  bookmarkSharp,
+  logOutOutline,
+  imageOutline,
+  imageSharp,
+  libraryOutline,
+  librarySharp,
+  logOutSharp,
+  calendarOutline,
+  calendarSharp,
+  mailOpenOutline,
+  mailOpenSharp,
+} from 'ionicons/icons';
+import { register } from 'swiper/element/bundle';
 
 register();
 @Component({
@@ -12,18 +50,66 @@ register();
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CommonModule, IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    CommonModule,
+    IonApp,
+    IonSplitPane,
+    IonMenu,
+    IonContent,
+    IonList,
+    IonListHeader,
+    IonNote,
+    IonMenuToggle,
+    IonItem,
+    IonIcon,
+    IonLabel,
+    IonRouterOutlet,
+  ],
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Articles', url: '/article', icon: 'bookmark'},
-    { title: 'Galeries d\'images', url: '/gal-images', icon: 'bookmark' },
-    { title: 'Dates Importantes', url: '/date-imp', icon: 'bookmark' },
-    { title: 'Informations de contact', url: '/info-contact', icon: 'bookmark' },
-
+    { title: 'Articles', url: '/article', icon: 'library' },
+    { title: "Galeries d'images", url: '/gal-images', icon: 'image' },
+    { title: 'Dates Importantes', url: '/date-imp', icon: 'calendar' },
+    {
+      title: 'Informations de contact',
+      url: '/info-contact',
+      icon: 'mail-open',
+    },
+    {
+      title: 'Déconnexion',
+      url: '/login',
+      icon: 'log-out',
+    },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {
-    addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
+    addIcons({
+      mailOutline,
+      mailSharp,
+      paperPlaneOutline,
+      paperPlaneSharp,
+      heartOutline,
+      heartSharp,
+      archiveOutline,
+      archiveSharp,
+      trashOutline,
+      trashSharp,
+      warningOutline,
+      warningSharp,
+      bookmarkOutline,
+      bookmarkSharp,
+      logOutOutline,
+      logOutSharp,
+      imageOutline,
+      imageSharp,
+      libraryOutline,
+      librarySharp,
+      calendarOutline,
+      calendarSharp,
+      mailOpenOutline,
+      mailOpenSharp,
+    });
   }
 }
